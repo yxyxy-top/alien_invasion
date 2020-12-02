31bfb2599389d34f1054278a87e3e64c256b016f
xuyu <xyz930906@163.com>
2020-12-02 18:06:23 +0800

    提交代码

--- alien.py
+++ alien.py
@@ -15,4 +15,13 @@ class Alien:
         self.image = pygame.image.load('images/alien.bmp')
         self.rect = self.image.get_rect()
 
-        # 每个外星人
+        # 每个外星人最初都在屏幕左上角附近
+        self.rect.x = self.rect.width
+        self.rect.y = self.rect.height
+
+        # 存储外星人的准确位置
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        """在指定位置绘制外星人"""
+        self.screen.blit(self.image, self.rect)
